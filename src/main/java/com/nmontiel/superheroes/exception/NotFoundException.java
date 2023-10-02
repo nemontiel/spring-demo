9851706916a8df4ecedf3b4253d2501f59dd6c2a
Nicolas Montiel <nemontiel@gmail.com>
2023-10-02 14:16:01 -0300

    add exception package

---
+++ src/main/java/com/nmontiel/superheroes/exception/NotFoundException.java
@@ -0,0 +1,15 @@
+package com.nmontiel.superheroes.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message){
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    public NotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
