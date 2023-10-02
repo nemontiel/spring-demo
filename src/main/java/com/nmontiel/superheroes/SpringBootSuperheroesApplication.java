fd87cd6c6c93726b818456595ff810cac87c51d7
Nicolas Montiel <nemontiel@gmail.com>
2023-10-02 14:16:55 -0300

    init

--- src/main/java/com/nmontiel/superheroes/SpringBootSuperheroesApplication.java
+++ src/main/java/com/nmontiel/superheroes/SpringBootSuperheroesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootSuperheroesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSuperheroesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSuperheroesApplication.class, args);
+    }
 
 }
