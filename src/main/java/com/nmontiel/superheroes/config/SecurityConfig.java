48d2aa643ff3d239cdb189ea8d25693c0ec4a83c
Nicolas Montiel <nemontiel@gmail.com>
2023-10-03 14:43:57 -0300

    add security config

---
+++ src/main/java/com/nmontiel/superheroes/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.nmontiel.superheroes.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests((authz) -> authz.requestMatchers("/**").permitAll().anyRequest().authenticated())
+            .httpBasic(withDefaults());
+        return http.build();
+    }
+}
